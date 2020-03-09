80ec527d1c89408191985e028d0a3fb5ea20bb38
AnthonyDem <thony.dem@gmail.com>
2020-03-09 12:19:19 +0300

    add password changing func

--- account/views.py
+++ account/views.py
@@ -1,25 +1,33 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from  django.contrib.auth import authenticate,login
-from .foms import LoginForm
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+
 
 def user_login(request):
-    if request.method=='POST':
-        form=LoginForm(request.POST)
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
         if form.is_valid():
-            cd =form.cleaned_data
-            user =authenticate(request,
-                               username=cd['username'],
-                               password=cd['password'])
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
         if user is not None:
             if user.is_active:
-                login(request,user)
+                login(request, user)
                 return HttpResponse('Authenticated successfully')
             else:
                 return HttpResponse('Disabled account')
         else:
             return HttpResponse('Invalid login/password')
     else:
-        form=LoginForm()
-    return render(request,'account/login.html',{'form':form})
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
 # Create your views here.
